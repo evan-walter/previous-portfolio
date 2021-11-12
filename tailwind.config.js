aee2bfd38fa83679e7bc1cc955b8684b0c8250b8
Evan <ewpw5678@gmail.com>
2021-11-12 12:30:21 -0800

    configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0c3483',
+        secondary: '#6b8cce',
+      },
+      fontFamily: {
+        body: ['Roboto'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

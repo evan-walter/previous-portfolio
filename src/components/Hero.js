f695e676a0ea3455c5efdb3b93efa4a71b4b0a4a
Evan <ewpw5678@gmail.com>
2021-11-12 21:25:18 -0800

    working with background image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,21 @@ import { DownArrow } from './Arrows';
 
 export default function Hero() {
   return (
-    <div>
+    <div
+      className='bg-opacity-30'
+      style={{
+        backgroundImage: 'url("../images/ewalter-3-2600.jpg")',
+      }}
+    >
       <h2>Hello, my name is Evan.</h2>
       <h2>I am a Software Engineer specializing in React.</h2>
       <div className='cursor-pointer' style={{ fontSize: '32px' }}>
-        <div><i className='fa fa-github' /></div>        
-        <div><i className='fa fa-linkedin' /></div> 
+        <div>
+          <i className='fa fa-github' />
+        </div>
+        <div>
+          <i className='fa fa-linkedin' />
+        </div>
       </div>
       <div className='cursor-pointer'>
         <i className='fa fa-angle-down fa-2x' />

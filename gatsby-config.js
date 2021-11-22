3a276662a898391e6750b3fcc8ddc5e34b79d54b
Evan <ewpw5678@gmail.com>
2021-11-21 21:58:33 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Evan Walter | Software Engineer",
+  },
+  plugins: ["gatsby-plugin-react-helmet"],
+};

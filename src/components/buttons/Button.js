6caf08c7e70d08bc289d94b62b58b80673f04b6f
Evan <ewpw5678@gmail.com>
2021-11-28 02:26:49 -0500

    style: eliminate comments and clean up

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 export default function Button({ textB, textNoB, addClassNames }) {
-  // const base = 'font-bold cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary ';
-  // const bBase = base + 'p-4 m-auto h-8 w-max flex items-center btn ';
-  // const textBCName = bBase;
-  // const textNoBCName = base;
-  const textNoBCName = 'font-bold cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary ';
+  const textNoBCName = 'font-bold cursor-pointer text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary';
+  
+  function more() {return addClassNames ? addClassNames : ''}
 
   return (
     <div>
-      {textB && <button className={'btn ' + (addClassNames ? addClassNames : '')}>{textB}</button>}
-      {textNoB && <button className={textNoBCName + ' ' + (addClassNames ? addClassNames : '')}>{textNoB}</button>}
+      {textB && <button className={'btn ' + more}>{textB}</button>}
+      {textNoB && <button className={textNoBCName + ' ' + more}>{textNoB}</button>}
     </div>
   );
 }

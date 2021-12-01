b0e842d85048406e5ca9ecf3e0653f0b7417778e
Evan <ewpw5678@gmail.com>
2021-12-01 14:49:58 -0800

    style: responsive navbar progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import React from 'react';
-// import Button from './buttons/Button';
-// import MyLink from './buttons/MyLink';
-// import logo from '../images/logo.png';
+import Button from './buttons/Button';
+import MyLink from './buttons/MyLink';
+import logo from '../images/logo.png';
 
 export default function Navbar() {
+  const links = ['home', 'about', 'projects', 'testimonials', 'contact'];
+
   function handleClick() {window.location.reload();}
 
   return (
-    <div className='bg-indigo-700 shadow-lg'>
-      <div className='container mx-auto'>
+    <div id='home'>
+      <div className='py-4 px-44 mx-auto'>
         <div className='sm:flex justify-around'>
-          {/* Site Title */}
-          <div className='text-white text-3xl font-bold p-3'>WEB ZONE</div>
-          {/* Menu Items */}
+          {/* Logo */}
+          {/* <div className='text-white text-3xl font-bold p-3'>LOGO</div> */}
+          <button onClick={handleClick} onKeyDown={handleClick} className='cursor-pointer'>
+            <img src={logo} alt='logo' />
+          </button>
+          {/* Nav Links */}
           <ul className='text-gray-400 sm:self-center text-xl border-t sm:border-none'>
-            <li className='sm:inline-block'>
-              <a className='p-3 hover:text-white' href='#'>Home</a>
-            </li>
-            <li className='sm:inline-block'>
-              <a className='p-3 hover:text-white' href='#'>About</a>
-            </li>
+            {links.map(
+              (value) => {
+                return (
+                  <li className='sm:inline-block'>
+                    <div className='p-3 transform hover:-translate-y-1 transition duration-200 ease-in'>
+                      <MyLink to={value}>
+                        <Button textNoB={value.charAt(0).toUpperCase() + value.slice(1)} />
+                      </MyLink>
+                    </div>
+                  </li>
+                )
+              }
+            )}
           </ul>
         </div>
       </div>

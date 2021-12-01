abd23b585cdecec56b3e382d4d6772a838d69471
Evan <ewpw5678@gmail.com>
2021-12-01 14:00:10 -0800

    style: more responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,14 @@ export default function Navbar() {
           {/* Site Title */}
           <div className='text-white text-3xl font-bold p-3'>WEB ZONE</div>
           {/* Menu Items */}
-          <div className='text-gray-400 sm:self-center text-xl border-t sm:border-none'>
-            <div className='sm:inline-block'>
-              <div className='p-3 hover:text-white'>Home</div>
-            </div>
-          </div>
+          <ul className='text-gray-400 sm:self-center text-xl border-t sm:border-none'>
+            <li className='sm:inline-block'>
+              <a className='p-3 hover:text-white' href='#'>Home</a>
+            </li>
+            <li className='sm:inline-block'>
+              <a className='p-3 hover:text-white' href='#'>About</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>

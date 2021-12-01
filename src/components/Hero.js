724416dc0e1c88e1d9864e42ee17ff4ea90a2d00
Evan <ewpw5678@gmail.com>
2021-11-30 20:22:59 -0700

    style: responsive hero and fonts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,13 @@ import Button from './buttons/Button';
 import Socials from './buttons/Socials';
 
 export default function Hero({ next }) {
-  const text = 'text-4xl font-bold py-4 leading-relaxed';
+  const text = 'text-xl sm:text-3xl lg:text-4xl text-center lg:text-left font-bold py-4 leading-relaxed';
   // const name = 'text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary';
 
   return (
     <div>
-      <div className='grid grid-cols-1 lg:grid-cols-2'>
-        <div>
+      <div className='grid grid-cols-1 lg:grid-cols-2'> {/* justify-self-center lg:justify-self-start */}
+        <div className='text-center lg:text-left'>
           <h2 className={text + ' '}>Hi, my name is Evan Walter.</h2> {/* 'text-5xl font-bold' */} {/* <h2 className={text + ' '}>Hi, my name is <span className={name}>Evan Walter</span>.</h2> */}
           <h2 className={text}>
             I am a Software Engineer specializing in React.
@@ -23,7 +23,7 @@ export default function Hero({ next }) {
             height='315'
             // width='590'
             // height='345'
-            className='hero-vid'
+            className='hero-vid m-auto'
             title='Welcome Video'
             frameBorder='0'
             allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'

5ec6555c2cc60b6c0797acae99fa392dc2ded6a1
Evan <ewpw5678@gmail.com>
2021-12-01 19:43:00 -0800

    style: ahh. finally found a good solution for a responsive navbar with the creator of tailwind!!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,31 +14,36 @@ export default function Navbar() {
   }
 
   return (
-    <div id='home' class='py-4 px-8 sm:px-36 xl:px-44'>
-      <div className='flex'>
-        <div class='flex-none'>
-          <button onClick={handleLogo} onKeyDown={handleLogo} className='cursor-pointer'>
-            <img src={logo} alt='logo' />
-          </button>
-        </div>
-        <div class='flex-grow'></div>
-        <div class='flex-none'>
-          <ul className=''> {/* hidden lg:visible m-auto grid grid-cols-1 lg:grid-cols-3 grid-flow-row */}
-            {links.map(
-              (value) => {
-                return (
-                  <li className='inline-block'> {/* lg:inline-block */}
-                    <div className='p-3'> {/* transform hover:-translate-y-1 transition duration-200 ease-in */}
-                      <MyLink to={value}>
-                        <Button textNoB={value.charAt(0).toUpperCase() + value.slice(1)} />
-                      </MyLink>
-                    </div>
-                  </li>
-                )
-              }
+    <div id='home' class='py-4 px-8 sm:px-36 xl:px-44 flex items-center justify-between'>
+      <div className=''>
+        <button onClick={handleLogo} onKeyDown={handleLogo} className='cursor-pointer'>
+          <img src={logo} alt='logo' />
+        </button>
+      </div>
+      <div className='bg-gray-300'>
+        <button className='text-gray-600 hover:text-gray-500 focus:text-gray-500'>
+          <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
+            <path d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
+            <path d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
+          </svg>
+        </button>
+      </div>
+      <div className=''>
+        <ul className=''>
+          {links.map(
+            (value) => {
+              return (
+                <li className=''>
+                  <div className='transform hover:-translate-y-1 transition duration-200 ease-in'>
+                    <MyLink to={value}>
+                      <Button textNoB={value.charAt(0).toUpperCase() + value.slice(1)} />
+                    </MyLink>
+                  </div>
+                </li>
+              )
+            }
             )}
-          </ul>
-        </div>
+        </ul>
       </div>
     </div>
   );

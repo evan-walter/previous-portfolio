0522826e5c803417d13ad8e82fea184fa79b733f
Evan <ewpw5678@gmail.com>
2021-12-02 11:42:04 -0800

    style: responsive navbar working with transition

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,10 @@ export default function Navbar() {
     window.location.reload();
   }
 
+  function handleBars() {
+    setExpanded(s => !s);
+  }
+
   return (
     <header id='home' class='bg-gray-100'>
       <div class='flex items-center justify-between py-4 px-8 sm:px-36 xl:px-44'>
@@ -22,21 +26,24 @@ export default function Navbar() {
           </button>
         </div>
         <div className=''>
-          <button className='text-gray-600 hover:text-gray-500 focus:text-gray-500'>
+          <button 
+            className='text-gray-600 hover:text-gray-500 focus:text-gray-500'
+            onClick={handleBars}
+          >
             <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
-              <path d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
-              <path d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
+              <path className={(expanded ? 'block' : 'hidden')} d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
+              <path className={(expanded ? 'hidden' : 'block')} d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
             </svg>
           </button>
         </div>
       </div>
-      <div className=''>
+      <div className={'pb-4 ' + 'transform translate-y-0 transition duration-300 ease-in ' + (expanded ? 'block' : 'transform translate-y-full transition duration-300 ease-in') }>
         <ul className=''>
           {links.map(
             (value) => {
               return (
                 <li className=''>
-                  <div className='transform hover:-translate-y-1 transition duration-200 ease-in'>
+                  <div className='block text-center py-2 transform hover:-translate-y-1 transition duration-200 ease-in'>
                     <MyLink to={value}>
                       <Button textNoB={value.charAt(0).toUpperCase() + value.slice(1)} />
                     </MyLink>

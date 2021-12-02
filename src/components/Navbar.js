3313bdfbfdf2cfce60d854238de441c37ff71458
Evan <ewpw5678@gmail.com>
2021-12-01 16:46:53 -0800

    style: responsive navbar, pausing with flex basis and refactoring to tailwind flex grow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './buttons/Button';
 import MyLink from './buttons/MyLink';
 import logo from '../images/logo.png';
 
 export default function Navbar() {
   const links = ['home', 'about', 'projects', 'testimonials', 'contact'];
+  const mobileCName = expanded ? 'expanded' : '';
 
-  function handleClick() {window.location.reload();}
+  const [expanded, setExpanded] = useState(false);
+
+  function handleLogo() {
+    window.location.reload();
+  }
+
+  function handleBars() {
+    setExpanded(s => !s);
+  }
 
   return (
-    <div id='home' class='flex container-my-nav py-4 px-8 sm:px-36 xl:px-44'>
-      <div class='one'>
-        <button onClick={handleClick} onKeyDown={handleClick} className='one cursor-pointer'>
+    <div id='home' className='flex container-my-nav py-4 px-8 sm:px-36 xl:px-44'>
+      <div className='one'>
+        <button onClick={handleLogo} onKeyDown={handleLogo} className='one cursor-pointer'>
           <img src={logo} alt='logo' />
         </button>
       </div>
-      <div class='two text-lg'>
-        <ul>
+      <div className='two text-lg'>
+        <div className='visible lg:invisible' onClick={handleBars}>
+          <i className='fa fa-bars' aria-hidden='true'></i>
+        </div>
+        <ul className=''> {/* m-auto grid grid-cols-1 lg:grid-cols-3 grid-flow-row */}
           {links.map(
             (value) => {
               return (
-                <li className='sm:inline-block'>
-                  <div className='p-3 transform hover:-translate-y-1 transition duration-200 ease-in'>
+                <li className='invisible lg:visible lg:inline-block'> {/* sm:inline-block */}
+                  <div className='p-3'> {/* transform hover:-translate-y-1 transition duration-200 ease-in */}
                     <MyLink to={value}>
                       <Button textNoB={value.charAt(0).toUpperCase() + value.slice(1)} />
                     </MyLink>

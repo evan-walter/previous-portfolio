d52abfcf2874cf9e63c6b2f68180e70475fa0462
Evan <ewpw5678@gmail.com>
2021-12-04 15:16:06 -0800

    feat: adding hero copy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import A from './buttons/A';
 import MyLink from './buttons/MyLink';
 import Button from './buttons/Button';
 import Socials from './buttons/Socials';
@@ -9,9 +10,11 @@ export default function Hero({ next }) {
   const textL = 'text-2xl sm:text-4xl lg:text-5xl ';
   const gradient = 'text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary'; // pr-0.5
 
+  const textLink = textBase;
+
   return (
     <div>
-      <div className='grid grid-cols-1 lg:grid-cols-2'> {/* justify-self-center lg:justify-self-start */}
+      <div className='grid grid-cols-1 gap-2 lg:grid-cols-2'> {/* justify-self-center lg:justify-self-start */}
         <div className='text-center lg:text-left'>
           <h2 className={textBase + textL + gradient}>Evan Walter</h2> {/* <h2 className={text}>Hi, my name is <span className={gradient}>Evan Walter</span></h2> */}
           <h2 className={textBase + textN}>
@@ -34,10 +37,21 @@ export default function Hero({ next }) {
           />
         </div>
       </div>
-      <div className='pt-16 pb-2 text-center'>
-      <MyLink to={next}>
-        <Button textB='Know more' addClassName='text-lg lg:text-xl' />
-      </MyLink>
+      <div className={'pt-12 text-lg text-center ' + gradient}>I built this portfolio from scratch with{' '}
+        <A h='https://reactjs.org/' addClassName={textLink}>React,</A>
+        {' '}
+        <A h='https://gatsbyjs.com/' addClassName={textLink}>Gatsby,</A>
+        {' '}and{' '}
+        <A h='https://tailwindcss.com/' addClassName={textLink}>Tailwind</A>
+        . Check out the source code{' '}
+        <A h='https://github.com/Evan-Walter/portfolio/' addClassName={textLink}>
+          here
+        </A>.
+      </div>
+      <div className='pt-12 pb-2 text-center'>
+        <MyLink to={next}>
+          <Button textB='Know more' />
+        </MyLink>
       </div>
     </div>
   );
--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default function Button({ textB, textNoB, noCursorPointer, addClassName }) {
-  const btnBBase = 'py-2 px-5 font-bold inline-block relative leading-none z-10 ';
-  const btnBDefaultSize = 'text-2xl ';
+  const btnBBase = 'font-bold inline-block relative leading-none z-10 ';
+  const btnBDefaultSize = 'py-1 px-4 text-lg lg:text-xl ';
   const btnNoB = 'font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary ';
   
   const more = function() {

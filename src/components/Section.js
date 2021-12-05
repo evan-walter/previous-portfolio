abc61c934b5e89d26301f2b0d4f6160efff0af94
Evan <ewpw5678@gmail.com>
2021-12-04 18:26:19 -0800

    feat: working on background image

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,10 +14,19 @@ export default function Section({
 }) {
   
   const containerCName = 'section px-8 py-16 sm:px-36 xl:px-44 grid grid-cols-1 self-center ' +
-    (bgImg ? 'bg-fixed' : '') +
+    (bgImg ? 'bg-img bg-img-hero' : '') +
     (bgColored ? 'bg-gradient-to-br from-primary to-secondary text-white ' : '');
 
-  const containerStyle = bgImg ? { backgroundImage : `url(../images/${bgImg})` } : {};
+  const containerStyle = bgImg ? {
+    height : '100%',
+    width : '100%',
+    content : '',
+    backgroundPosition : 'center',
+    backgroundRepeat : 'noRepeat',
+    backgroundAttachment : 'fixed',
+    backgroundSize : 'cover',
+    backgroundImage  : `url(../images/${bgImg}` }
+    : {};
 
   const tBaseCName = 'pb-8 lg:pb-12 text-3xl lg:text-4xl font-bold text-center';
   const tGradientCName = ' text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary';
@@ -26,7 +35,7 @@ export default function Section({
     (titleGradient ? (tBaseCName + tGradientCName) : '');
   
   return (
-    <div id={id} className={containerCName} style={containerStyle}>
+    <div id={id} className={containerCName}> {/**style={containerStyle} */}
       <div className={titleCName}>
         {titleShown}
       </div>

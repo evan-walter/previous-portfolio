13b8f292c7c25d414d213d4f9538f313b5f99a1c
Evan <ewpw5678@gmail.com>
2021-12-04 19:06:51 -0800

    feat: working background images for hero and contact sections!

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,27 +6,15 @@ export default function Section({
   id,
   next,
   last,
-  bgColored,
-  bgImg,
   titleShown,
   titleGradient,
+  bgColored,
+  bgImg,
   children,
 }) {
   
   const containerCName = 'section px-8 py-16 sm:px-36 xl:px-44 grid grid-cols-1 self-center ' +
-    (bgImg ? 'bg-img bg-img-hero' : '') +
     (bgColored ? 'bg-gradient-to-br from-primary to-secondary text-white ' : '');
-
-  // const containerStyle = bgImg ? {
-  //   height : '100%',
-  //   width : '100%',
-  //   content : '',
-  //   backgroundPosition : 'center',
-  //   backgroundRepeat : 'noRepeat',
-  //   backgroundAttachment : 'fixed',
-  //   backgroundSize : 'cover',
-  //   backgroundImage  : `url(../images/${bgImg}` }
-  //   : {};
 
   const tBaseCName = 'pb-8 lg:pb-12 text-3xl lg:text-4xl font-bold text-center';
   const tGradientCName = ' text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary';
@@ -35,7 +23,7 @@ export default function Section({
     (titleGradient ? (tBaseCName + tGradientCName) : '');
   
   return (
-    <section id={id} className={containerCName}> {/**className={containerCName} style={containerStyle} */}
+    <section id={id} className={containerCName}> {/**style={containerStyle} */}
       <div className={titleCName}>
         {titleShown}
       </div>

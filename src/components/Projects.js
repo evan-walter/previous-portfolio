e0dd7d9e74fafeee3cdb8b85680cba0b709ec0e8
Evan <ewpw5678@gmail.com>
2021-12-07 19:20:34 -0800

    another attempt

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,29 +24,36 @@ export default function Projects(props) {
   return (
     <div>
       {showModal ?
-        <div className='z-30 py-16 px-36 w-screen fixed inset-0 bg-black bg-opacity-60'>
-          <div onClick={handleModal}>
+        <div className='z-30 px-16 w-screen fixed inset-0 bg-black bg-opacity-60'>
+          {/* <div onClick={handleModal}>
             <div className='z-40 w-screen h-16 fixed inset-x-0 top-0 bg-transparent'></div>
             <div className='z-40 h-screen w-36 fixed inset-y-0 right-0 bg-transparent'></div>
             <div className='z-40 w-screen h-16 fixed inset-x-0 bottom-0 bg-transparent'></div>
             <div className='z-40 h-screen w-36 fixed inset-y-0 left-0 bg-transparent'></div>
-          </div>
-          <div className='flex items-center'>
-            <div className='z-50 w-full border-2 border-black'>
-              <div className='p-2 m-auto bg-white flex items-center justify-end border-b border-gray-400'>
-                <button onClick={handleModal}>
-                  <i className='fa fa-times' aria-hidden='true' />
-                </button>
-              </div>
-              <div className='flex justify-center'>
-                <div className='video-container'>
-                  <iframe
-                    className='responsive-iframe'
-                    src={whichVideo}
-                  ></iframe>
+          </div> */}
+          {/* <div className='grid grid-rows-3'>
+            <div className='row-span-1'>1</div>
+            <div className='flex items-center'>
+              <div className='z-50 w-full border-2 border-black'>
+                <div className='p-2 m-auto bg-white flex items-center justify-end border-b border-gray-400'>
+                  <button onClick={handleModal}>
+                    <i className='fa fa-times' aria-hidden='true' />
+                  </button>
+                </div>
+                <div className='flex justify-center'>
+                  <div className='video-container'>
+                    <iframe
+                      className='responsive-iframe'
+                      src={whichVideo}
+                      ></iframe>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className=''>3</div>
+          </div> */}
+          <div className='relative video-padding'>
+            <iframe className='absolute inset-0 w-full h-full' src={whichVideo} frameborder='0' />
           </div>
         </div> : null
       }

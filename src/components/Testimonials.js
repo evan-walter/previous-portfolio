c20807679ac05ae80d3a8991fdca73f3f56daaf2
Evan Walter <ewpw5678@gmail.com>
2022-01-20 18:20:17 -0800

    feat: add photo to testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Fade from './Fade';
+import meganImg from '../assets/images/megan.jpg'
 
 export default function Testimonials() {
-  const p = 'p-4 md:text-lg lg:text-xl text-center';
+  const p = 'p-4 md:text-lg lg:text-xl lg:max-w-lg text-center';
   return (
     <div className='m-auto lg:max-w-5xl'>
       <Fade from='bottom'>
-        <p className={p}>
-          "Evan is a hard worker and is extremely savvy. Even if he doesn't know
-          how to do something, he is going to get creative and figure it out,
-          which is one of the best qualities in a developer. He takes care with
-          his work and is extremely communicative. I would highly recommend Evan
-          for your projects."
-        </p>
+        <div>
+          <img src={meganImg} className='rounded-full m-auto' alt='Megan Spaulding' width='100' />
+        </div>
+          <p className={p}>
+            "Evan is a hard worker and is extremely savvy. Even if he doesn't know
+            how to do something, he is going to get creative and figure it out,
+            which is one of the best qualities in a developer. He takes care with
+            his work and is extremely communicative. I would highly recommend Evan
+            for your projects."
+          </p>
       </Fade>
       <Fade from='bottom'>
         <p className={p + ' italic'}> — Megan Spaulding</p>
